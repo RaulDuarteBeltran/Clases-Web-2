de6e5fd6622b33d79945d11a2a50af5d975bed31
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2026-03-04 15:51:34 -0600

    Inicialización de base de datos

---
+++ sqlmodel/main.py
@@ -0,0 +1,18 @@
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def main():
+    create_db_and_tables()
+
+
+if __name__ == "__main__":
+    main()
