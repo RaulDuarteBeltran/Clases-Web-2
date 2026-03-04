dcd012585893f70137ffdea9beffa4d3dfee6857
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2026-03-04 15:54:08 -0600

    Creación de tablas Team y Hero

--- sqlmodel/main.py
+++ sqlmodel/main.py
@@ -6,6 +6,23 @@ class HeroTeamLink(SQLModel, table=True):
     hero_id: int | None = Field(default=None, foreign_key="hero.id", primary_key=True)
 
 
+class Team(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
+
+    heroes: list["Hero"] = Relationship(back_populates="teams", link_model=HeroTeamLink)
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    secret_name: str
+    age: int | None = Field(default=None, index=True)
+
+    teams: list[Team] = Relationship(back_populates="heroes", link_model=HeroTeamLink)
+
+
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
