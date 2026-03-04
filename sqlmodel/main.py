9dc74c976391388b8e4cd3c6d9c1ab7659ca3e69
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2026-03-04 15:53:39 -0600

    Creación de tabla de asociación

--- sqlmodel/main.py
+++ sqlmodel/main.py
@@ -1,5 +1,11 @@
 from sqlmodel import Field, Relationship, Session, SQLModel, create_engine
 
+
+class HeroTeamLink(SQLModel, table=True):
+    team_id: int | None = Field(default=None, foreign_key="team.id", primary_key=True)
+    hero_id: int | None = Field(default=None, foreign_key="hero.id", primary_key=True)
+
+
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
